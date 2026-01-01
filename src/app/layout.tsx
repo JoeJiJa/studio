e85ffcd04a7d086a7abfff7c42c72d0cf1b9886a
Ads <ads.com.jai@gmail.com>
2026-01-01 04:40:38 +0000

    Increase the width, as there are more empty space on both the side. Make

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
           </div>
           <div className="flex min-h-screen">
             <main className="flex-grow pb-16">
-              <div className="container mx-auto px-4 sm:px-6 py-8 max-w-lg lg:max-w-4xl">
+              <div className="container mx-auto px-4 sm:px-6 py-8 max-w-lg lg:max-w-7xl">
                 {children}
               </div>
             </main>
