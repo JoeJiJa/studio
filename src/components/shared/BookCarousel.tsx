5c3837f5146b7186a8d08a473a20a01b465f8edc
Ads <ads.com.jai@gmail.com>
2026-01-01 04:20:39 +0000

    A little (Very low amount) decrease the width of the carousel sliders al

--- src/components/shared/BookCarousel.tsx
+++ src/components/shared/BookCarousel.tsx
@@ -79,7 +79,7 @@ export function BookCarousel({
           const isExternal = href.startsWith('http');
 
           return (
-            <CarouselItem key={id} className="basis-1/4 sm:basis-1/6 md:basis-1/8 lg:basis-1/10 xl:basis-1/12 pl-1">
+            <CarouselItem key={id} className="basis-1/3 sm:basis-1/5 md:basis-1/7 lg:basis-1/9 xl:basis-1/11 pl-1">
               <div className="p-0.5">
                 <Link 
                   href={href} 
