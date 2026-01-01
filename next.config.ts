ba618a5deda3780fee24bbf59e86e2bcfdd23819
Ads <ads.com.jai@gmail.com>
2026-01-01 09:33:49 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://www.feb

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'httpshttps',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -128,6 +128,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'www.kenhub.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.febbox.com',
         port: '',
         pathname: '/**',
       },
