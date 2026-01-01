66a9f5222c00b0a33a957b7324e3012ae8b6d5fc
Ads <ads.com.jai@gmail.com>
2026-01-01 07:18:36 +0000

    fix the splash screen background. I want the background of the splash sc

--- src/components/shared/SplashScreen.tsx
+++ src/components/shared/SplashScreen.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 export function SplashScreen() {
   return (
-    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-background animate-splash-fade-out">
+    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black animate-splash-fade-out">
       <div className="relative w-48 h-48 animate-splash-pop-in">
         <Image
           src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/jogRQQ7NL14vh4LDNK8n/pub/fRGo42eAcDofrKwh53zL/Dr%20Astro.jpg"
