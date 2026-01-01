fbc7e82b222c1a10d667e24fc5a675e5f42ee82d
Ads <ads.com.jai@gmail.com>
2026-01-01 04:37:33 +0000

    I want the carousel sliders have 2 images of the book to be visible. Oth

--- src/components/shared/BookCarousel.tsx
+++ src/components/shared/BookCarousel.tsx
@@ -79,7 +79,7 @@ export function BookCarousel({
           const isExternal = href.startsWith('http');
 
           return (
-            <CarouselItem key={id} className="basis-full sm:basis-4/5 md:basis-3/5 lg:basis-1/2 pl-1">
+            <CarouselItem key={id} className="basis-1/2 pl-1">
               <div className="p-0.5">
                 <Link 
                   href={href} 
