6d38cc569953cdb185a7bc217a2726dd43c9f759
Ads <ads.com.jai@gmail.com>
2026-01-01 04:38:15 +0000

    Decrease the wisdth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
           </div>
           <div className="flex min-h-screen">
             <main className="flex-grow pb-16">
-              <div className="container mx-auto px-4 sm:px-6 py-8 max-w-xl">
+              <div className="container mx-auto px-4 sm:px-6 py-8 max-w-lg">
                 {children}
               </div>
             </main>
